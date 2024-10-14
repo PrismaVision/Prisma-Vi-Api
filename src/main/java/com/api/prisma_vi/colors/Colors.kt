acad2bfee21dadf93816db2f5551210cb5690f16
SamuelSoaresSilva <samuel.soares.cunha64@gmail.com>
2024-10-14 00:43:34 -0300

    Create color table

---
+++ src/main/java/com/api/prisma_vi/colors/Colors.kt
@@ -0,0 +1,21 @@
+package com.api.prisma_vi.colors
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+data class Colors(
+
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    val id: Int,
+    val name: String,
+    val hexCode: String,
+    val rgbCode: String,
+//    val rybPercentages: String,
+    val colorDescription: String,
+    val twoColorsThatMatchHex: String,
+    val colorTerminology: String
+
+)

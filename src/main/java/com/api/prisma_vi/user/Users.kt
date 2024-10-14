de0c7b49587d519d78d6338940b5282be2a94c75
SamuelSoaresSilva <samuel.soares.cunha64@gmail.com>
2024-10-14 00:49:41 -0300

    Create Users class

--- src/main/java/com/api/prisma_vi/user/Users.kt
+++ src/main/java/com/api/prisma_vi/user/Users.kt
@@ -1,12 +1,17 @@
 package com.api.prisma_vi.user
 
+import com.api.prisma_vi.colors.Colors
+import com.api.prisma_vi.palette.Palette
 import jakarta.persistence.*
 
-@Entity(name = "USERS")
+@Entity
 data class Users(
 
     @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
     val id: Int,
+
+    @Column(nullable = false)
+    val name: String,
 
     @Column(unique = true)
     val login: String,
@@ -14,8 +19,9 @@ data class Users(
     @Column(nullable = false)
     val password: String,
 
-    @Column(nullable = false)
-    val role: String
+    @OneToMany
+    val palettes: List<Palette>,
 
-
+    @ManyToOne
+    val colorHistory: List<Colors>
 )

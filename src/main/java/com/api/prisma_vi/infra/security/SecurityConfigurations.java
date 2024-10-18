82879e25cb8805bcf2f0c72f7a0e545ad15c899d
SamuelSoaresSilva <samuel.soares.cunha64@gmail.com>
2024-10-18 15:04:27 -0300

    Service is running update

--- src/main/java/com/api/prisma_vi/infra/security/SecurityConfigurations.java
+++ src/main/java/com/api/prisma_vi/infra/security/SecurityConfigurations.java
@@ -28,6 +28,7 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, "api/is-running").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)

2a986ce8ac0712395d4f00b448596406f032f563
Samuel Soares Silva <e.samuel.csilva@mpt.mp.br>
2024-10-29 15:53:53 -0300

    Create validations

--- src/main/java/com/api/prisma_vi/auth/register/RegisterService.kt
+++ src/main/java/com/api/prisma_vi/auth/register/RegisterService.kt
@@ -11,16 +11,40 @@ import org.springframework.stereotype.Service
 @Service
 class RegisterService(private val repository: UsersRepository) {
 
-    fun registerUser(data: RegisterForm): ResponseEntity<Any>{
+    fun registerUser(data: RegisterForm): ResponseEntity<Any?>{
+        return when{
 
+            data.nickName.contains(" ") || data.password.contains(" ") ->
+                ResponseEntity.status(HttpStatus.CONFLICT).body("Input field can not have spaces")
 
-        if (repository.existsByEmail(data.email))
-            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("There is already an account linked to this email")
+            data.password.length < 6 || data.password.length > 16 ->
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Password must contain 6 to 16 characters")
 
-        val encryptedPassword: String = BCryptPasswordEncoder().encode(data.password);
-        val newUser = Users(data.nickName, data.email, encryptedPassword, UserRole.USER)
+            (repository.existsByEmail(data.email)) ->
+                ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("There is already an account linked to this email")
 
-        repository.save(newUser)
-        return ResponseEntity.ok().build()
+         else ->{
+            ResponseEntity.ok().body(saveUser(data))
+            }
+        }
+    }
+    private fun saveUser(data: RegisterForm): String{
+
+        val role: UserRole = if (data.email == "prisma@prismatic"){
+            UserRole.ADMIN
+        } else {
+            UserRole.USER
+        }
+
+        val encryptedPassword: String = BCryptPasswordEncoder().encode(data.password)
+        repository.save(
+            Users(
+                data.nickName,
+                data.email,
+                encryptedPassword,
+                role
+            )
+        )
+        return "${data.nickName} was registered successfully"
     }
 }
--- src/main/java/com/api/prisma_vi/user/Users.java
+++ src/main/java/com/api/prisma_vi/user/Users.java
@@ -2,7 +2,6 @@ package com.api.prisma_vi.user;
 
 import com.api.prisma_vi.palette.Palette;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;

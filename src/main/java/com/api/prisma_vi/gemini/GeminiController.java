110c15d6571d2f704c7baa5dddf73f5daed21d37
Prisma-VI <prismaviorg@gmail.com>
2024-11-05 23:33:25 -0300

    Refactor GeminiController
    Signed-off-by: Prisma-VI <prismaviorg@gmail.com>

--- src/main/java/com/api/prisma_vi/gemini/GeminiController.java
+++ src/main/java/com/api/prisma_vi/gemini/GeminiController.java
@@ -1,6 +1,9 @@
 package com.api.prisma_vi.gemini;
 
+import com.api.prisma_vi.apiError.InvalidHexadecimalException;
+import com.api.prisma_vi.colors.ColorsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,14 +13,23 @@ public class GeminiController {
     private final GeminiService geminiService;
 
     @Autowired
+    private ColorsService colorsService;
+
+    @Autowired
     public GeminiController(GeminiService geminiService) {
         this.geminiService = geminiService;
     }
 
     @PostMapping
-    public String generate(@RequestBody String hex) {
-        return geminiService.formatResponse(
-                geminiService.generateContent(
-                        geminiService.generatePrompt(hex.trim())));
+    public ResponseEntity<?> generate(@RequestBody String hex) {
+        try{colorsService.validateHexColor(hex);}
+        catch (InvalidHexadecimalException e){
+         return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return ResponseEntity.ok().body(
+                geminiService.formatResponse(
+                        geminiService.generateContent(
+                                geminiService.generatePrompt(hex.trim())))
+        );
     }
 }

db2d58248b285649c9190bd076131ff871ddc7d5
Samuel Soares <samuel.soares.cunha64@gmail.com>
2025-04-09 14:59:23 -0300

    create calculateLuminance method

--- src/main/java/com/api/prisma_vi/colors/ColorsService.java
+++ src/main/java/com/api/prisma_vi/colors/ColorsService.java
@@ -14,4 +14,18 @@ public class ColorsService {
             throw new InvalidHexadecimalException("Invalid hexadecimal color code");
         }
     }
+
+    public double calculateLuminance(int red, int green, int blue) {
+        return 0.2126 * channel(red) + 0.7152 * channel(green) + 0.0722 * channel(blue);
+    }
+
+    private double channel(int c) {
+        double cNorm = c / 255.0;
+        if (cNorm <= 0.03928) {
+            return cNorm / 12.92;
+        } else {
+            return Math.pow((cNorm + 0.055) / 1.055, 2.4);
+        }
+    }
+
 }

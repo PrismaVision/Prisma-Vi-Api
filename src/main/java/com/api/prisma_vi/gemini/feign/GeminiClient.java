b0bd8223ea7875592b58bddf3b84a7627b15ba75
Samuel Soares <samuel.soares.cunha64@gmail.com>
2025-04-09 14:52:25 -0300

    update gemini version to `2.0-flash`

--- src/main/java/com/api/prisma_vi/gemini/feign/GeminiClient.java
+++ src/main/java/com/api/prisma_vi/gemini/feign/GeminiClient.java
@@ -1,16 +1,17 @@
-package com.api.prisma_vi.gemini;
+package com.api.prisma_vi.gemini.feign;
 
+import com.api.prisma_vi.gemini.GeminiRequestBody;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "geminiClient", url = "https://generativelanguage.googleapis.com")
+@FeignClient(name = "geminiClient", url = "https://generativelanguage.googleapis.com", configuration = FeignConfig.class)
 public interface GeminiClient {
 
 
-    @PostMapping("/v1beta/models/gemini-1.5-flash-8b:generateContent")
+    @PostMapping("/v1beta/models/gemini-2.0-flash-lite:generateContent")
     ResponseEntity<String> searchColor(@RequestBody GeminiRequestBody body,
                                        @RequestParam("key") String apiKey);
 

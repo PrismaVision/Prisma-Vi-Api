28bad2b0845115009bc75e711f218fdc4ca164d9
Samuel Soares <samuel.soares.cunha64@gmail.com>
2025-06-11 15:12:16 -0300

    remove unused test

--- src/test/java/com/api/prisma_vi/PrismaViApplicationTests.java
+++ src/test/java/com/api/prisma_vi/PrismaViApplicationTests.java
@@ -1,13 +1,9 @@
 package com.api.prisma_vi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PrismaViApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }

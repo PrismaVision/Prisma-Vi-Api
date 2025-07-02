2bdd4b586fbac32152a219f140d14aadad346bda
Samuel Soares <samuel.soares.cunha64@gmail.com>
2025-07-02 16:53:09 -0300

    remove spring security and hibernate modules

--- src/main/java/com/api/prisma_vi/PrismaViApplication.java
+++ src/main/java/com/api/prisma_vi/PrismaViApplication.java
@@ -3,10 +3,9 @@ package com.api.prisma_vi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-@EnableJpaRepositories
+
 @SpringBootApplication
 @EnableFeignClients
 public class PrismaViApplication {
